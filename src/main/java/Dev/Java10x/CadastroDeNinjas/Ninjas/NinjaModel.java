b5f73fb4d74feb328a7eba44e1521424a4737dc6
Hapolo11 <151068338+Hapolo11@users.noreply.github.com>
2025-08-20 03:26:00 -0300

    fix: Coloca o atributo unique para o atributo email da Entidade Ninja

--- src/main/java/Dev/Java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/Dev/Java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -24,6 +24,7 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
    private String nome;
+   @Column(unique = true)
    private String Email;
    private int idade;
     // Um Ninja tem varias missões
